e10ba7ee8edbdd01289b5a5514f22219a9cd9a41
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2022-01-21 22:44:48 +0100

    Added + operator to and toString to MainStatComponent

--- RPGHelper.Models/Models/WarhammerFantasy/Character/Character Stats/MainStats/MainStatComponent.cs
+++ RPGHelper.Models/Models/WarhammerFantasy/Character/Character Stats/MainStats/MainStatComponent.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata;
+
 namespace RPGHelper.Context.Models.WarhammerFantasy.Character;
 
 public class MainStatComponent
@@ -11,4 +13,23 @@ public class MainStatComponent
     /// I have no idea, what it is ATM see: https://drive.google.com/file/d/1ysHjhJLLcBaDRUwq3VHC8FX-EAtqnXiD/view
     /// </summary>
     public int BoxAmount { get; set; }
+
+    public override string ToString()
+    {
+        var returnValue = String.Empty;
+        foreach (var VARIABLE in this.GetType().GetProperties())
+        {
+            returnValue += $"{VARIABLE.Name} : {VARIABLE.GetValue(this)}\n";
+        }
+        return returnValue;
+    }
+
+    public static MainStatComponent operator +(MainStatComponent a, MainStatComponent b)
+    {
+        if (a.StatTypeEnum != b.StatTypeEnum) throw new Exception("Wrong components added");
+        a.StartValue += b.StartValue;
+        a.AdvanceValue += b.AdvanceValue;
+        a.CurrentValue += b.CurrentValue;
+        return a;
+    }
 }
--- RPGHelper.TestChamber/Program.cs
+++ RPGHelper.TestChamber/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using RPGHelper.Context.Models.WarhammerFantasy.Character;
+
 Console.WriteLine("Hello, World!");
+
+MainStatComponent something = new()
+{
+    Name = "Cum",
+    StatTypeEnum = MainStatTypeEnum.WeaponSkill,
+    StartValue = 10,
+    AdvanceValue = 12,
+    CurrentValue = 123,
+    BoxAmount = 0
+};
+MainStatComponent somethingElse = new()
+{
+    Name = "chuj",
+    StatTypeEnum = MainStatTypeEnum.WeaponSkill,
+    StartValue = 3,
+    AdvanceValue = 4,
+    CurrentValue = 7,
+    BoxAmount = 0
+};
+Console.WriteLine(something.ToString());
+Console.WriteLine(somethingElse.ToString());
+something += somethingElse;
+Console.WriteLine(something);
