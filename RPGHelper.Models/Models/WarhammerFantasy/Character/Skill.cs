8d807264d490ba345c49822e557058241ddb4e50
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2022-01-21 13:25:05 +0100

    Added talents and skills models

--- RPGHelper.Models/Models/WarhammerFantasy/Character/Skill.cs
+++ RPGHelper.Models/Models/WarhammerFantasy/Character/Skill.cs
@@ -3,5 +3,8 @@ namespace RPGHelper.Context.Models.WarhammerFantasy.Character;
 public class Skill
 {
     public bool IsAdvanced { get; set; }
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public MainStatTypeEnum Characteristic { get; set; }
+    public List<Talent> RelatedTalents { get; set; }
 }
--- RPGHelper.Models/Models/WarhammerFantasy/Character/Talent.cs
+++ RPGHelper.Models/Models/WarhammerFantasy/Character/Talent.cs
@@ -2,5 +2,6 @@ namespace RPGHelper.Context.Models.WarhammerFantasy.Character;
 
 public class Talent
 {
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
 }
