8f00e31cfcab3cea2662ded2994bb3e0c7bf4ef3
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2022-01-22 18:28:05 +0100

    Did a lot of pdf->csv conversion

--- RPGHelper.TestChamber/Program.cs
+++ RPGHelper.TestChamber/Program.cs
@@ -4,27 +4,4 @@ using RPGHelper.Context.Models.WarhammerFantasy.Character;
 
 Console.WriteLine("Hello, World!");
 
-MainStatComponent something = new()
-{
-    Name = "Cum",
-    StatTypeEnum = MainStatTypeEnum.WeaponSkill,
-    StartValue = 10,
-    AdvanceValue = 12,
-    CurrentValue = 123,
-    BoxAmount = 0
-};
-MainStatComponent somethingElse = new()
-{
-    Name = "chuj",
-    StatTypeEnum = MainStatTypeEnum.WeaponSkill,
-    StartValue = 3,
-    AdvanceValue = 4,
-    CurrentValue = 7,
-    BoxAmount = 0
-};
-Console.WriteLine(something.ToString());
-Console.WriteLine(somethingElse.ToString());
-something += somethingElse;
-Console.WriteLine(something);
-something.AddTo("StartValue",-9000);
-Console.WriteLine(something);
+
