94d6b7ead744550fc2172c09ab5b7b5c1ef4d66e
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2022-01-23 11:48:29 +0100

    Updated WeaponDamage.cs model

--- RPGHelper.Models/Models/WarhammerFantasy/Items/Weapons/WeaponDamage.cs
+++ RPGHelper.Models/Models/WarhammerFantasy/Items/Weapons/WeaponDamage.cs
@@ -1,7 +1,10 @@
+using RPGHelper.Context.Models.WarhammerFantasy.Character;
+
 namespace RPGHelper.Models.Models.WarhammerFantasy.Items;
 
 public class WeaponDamage
 {
     public int Amount { get; set; }
     public bool IsRanged { get; set; }
+    public SecondaryStatTypeEnum? Base { get; set; }
 }
