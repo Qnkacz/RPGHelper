19a4c5d04815aa8f922557a427f676e53f54a3e6
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2022-02-01 12:47:08 +0100

    Fixed Skill typos

--- RPGHelper.Models/Models/WarhammerFantasy/Character/Character Stats/MainStats/MainStatTypeEnum.cs
+++ RPGHelper.Models/Models/WarhammerFantasy/Character/Character Stats/MainStats/MainStatTypeEnum.cs
@@ -3,11 +3,11 @@ namespace RPGHelper.Context.Models.WarhammerFantasy.Character;
 public enum MainStatTypeEnum
 {
     WeaponSkill=0,
-    BallsticSkill=1,
-    Strenght=2,
+    BallisticSkill=1,
+    Strength=2,
     Toughness=3,
     Agility=4,
-    Inteligence=5,
+    Intelligence=5,
     WillPower=6,
     Fellowship=7
 }
