118fa3c50ae22f607b02de6a16cd8a9d8cb3194a
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2022-02-01 21:30:15 +0100

    Sanitized skills csv files Added to skills covnerter in Skills.cs

--- RPGHelper.Functionality/Models/WarhammerFantasy/Skills.cs
+++ RPGHelper.Functionality/Models/WarhammerFantasy/Skills.cs
@@ -1,3 +1,7 @@
+using RPGHelper.Context.InfoTables.WarhammerFantasy;
+using RPGHelper.Context.Models.WarhammerFantasy.Character;
+using RPGHelper.Functionality.ExtensionMethods;
+
 namespace RPGHelper.Functionality.Models.WarhammerFantasy;
 
 public class Skills
@@ -13,5 +17,30 @@ public class Skills
         var path =
             "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/Skills/Advanced.csv_withId";
         return await Task.Run(()=>CSVHelper.GetDynamicFromCsvFile(path)) ?? throw new Exception("list was null");
+    }
+
+    public static Skill ConvertToSkill(dynamic obj)
+    {
+        var dbContext = new WarhammerContext();
+        Boolean.TryParse(obj.IsAdvanced, out bool IsAdvanced);
+        var talentList =((string[]) obj.RelatedTalents.Split("-")).ToList();
+        talentList.TrimAll();
+        var talentsFromDb = dbContext.Talents.ToList();
+        List<Talent> selectedTalents = new();
+        foreach (var s in talentList)
+        {
+            if(!string.IsNullOrEmpty(s))
+                selectedTalents.Add(talentsFromDb.First(talent=>talent.Name.Contains(s))); 
+        }
+        
+        Skill outputSkill = new()
+        {
+            IsAdvanced = IsAdvanced,
+            Name = obj.SkillName,
+            Description = obj.Description,
+            Characteristic = (MainStatTypeEnum) Enum.Parse(typeof(MainStatTypeEnum), obj.Characteristic,true),
+            RelatedTalents = selectedTalents
+        };
+        return outputSkill;
     }
 }
