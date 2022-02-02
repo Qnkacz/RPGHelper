79dc09c2c8276ce69994e3f8fa2b7105bc282bd3
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2022-02-02 12:30:53 +0100

    Fixed Advanced.csv_withId Added cinvert to skill list method in Skills.cs

--- RPGHelper.Functionality/Models/WarhammerFantasy/Skills.cs
+++ RPGHelper.Functionality/Models/WarhammerFantasy/Skills.cs
@@ -10,37 +10,67 @@ public class Skills
     {
         var path =
             "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/Skills/BasicSkills.csv_withId";
-        return await Task.Run(()=>CSVHelper.GetDynamicFromCsvFile(path)) ?? throw new Exception("list was null");
+        return await Task.Run(() => CSVHelper.GetDynamicFromCsvFile(path)) ?? throw new Exception("list was null");
     }
+
     public static async Task<List<dynamic>> GetAdvanced()
     {
         var path =
             "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/Skills/Advanced.csv_withId";
-        return await Task.Run(()=>CSVHelper.GetDynamicFromCsvFile(path)) ?? throw new Exception("list was null");
+        return await Task.Run(() => CSVHelper.GetDynamicFromCsvFile(path)) ?? throw new Exception("list was null");
     }
 
     public static Skill ConvertToSkill(dynamic obj)
     {
         var dbContext = new WarhammerContext();
         Boolean.TryParse(obj.IsAdvanced, out bool IsAdvanced);
-        var talentList =((string[]) obj.RelatedTalents.Split("-")).ToList();
+        var talentList = ((string[]) obj.RelatedTalents.Split("-")).ToList();
         talentList.TrimAll();
-        var talentsFromDb = dbContext.Talents.ToList();
         List<Talent> selectedTalents = new();
         foreach (var s in talentList)
         {
-            if(!string.IsNullOrEmpty(s))
-                selectedTalents.Add(talentsFromDb.First(talent=>talent.Name.Contains(s))); 
+            if (!string.IsNullOrEmpty(s))
+                selectedTalents.Add(dbContext.Talents.Find(s)!);
         }
-        
+
         Skill outputSkill = new()
         {
             IsAdvanced = IsAdvanced,
             Name = obj.SkillName,
             Description = obj.Description,
-            Characteristic = (MainStatTypeEnum) Enum.Parse(typeof(MainStatTypeEnum), obj.Characteristic,true),
+            Characteristic = (MainStatTypeEnum) Enum.Parse(typeof(MainStatTypeEnum), obj.Characteristic, true),
             RelatedTalents = selectedTalents
         };
         return outputSkill;
+    }
+
+    public static List<Skill> ConvertToSkillList(List<dynamic> list)
+    {
+        var dbContext = new WarhammerContext();
+        List<Skill> skillList = new();
+
+        foreach (var item in list)
+        {
+            Boolean.TryParse(item.IsAdvanced, out bool IsAdvanced);
+            var talentList = ((string[]) item.RelatedTalents.Split("-")).ToList();
+            talentList.TrimAll();
+            List<Talent> selectedTalents = new();
+            foreach (var s in talentList)
+            {
+                if (!string.IsNullOrEmpty(s))
+                    selectedTalents.Add(dbContext.Talents.Find(s)!);
+            }
+
+            skillList.Add(new Skill
+            {
+                IsAdvanced = IsAdvanced,
+                Name = item.SkillName,
+                Description = item.Description,
+                Characteristic = (MainStatTypeEnum) Enum.Parse(typeof(MainStatTypeEnum), item.Characteristic, true),
+                RelatedTalents = selectedTalents
+            });
+        }
+
+        return skillList;
     }
 }
