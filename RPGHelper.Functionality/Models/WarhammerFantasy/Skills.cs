4b61e9e72e27017154711603f69dce5e246d3c5f
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2022-02-04 13:24:26 +0100

    Completed careers simplified csv changed names to skills and talents

--- RPGHelper.Functionality/Models/WarhammerFantasy/Skills.cs
+++ RPGHelper.Functionality/Models/WarhammerFantasy/Skills.cs
@@ -9,14 +9,14 @@ public class Skills
     public static async Task<List<dynamic>> GetBasic()
     {
         var path =
-            "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/Skills/BasicSkills.csv_withId";
+            "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/Skills/BasicSkillsId.csv";
         return await Task.Run(() => CSVHelper.GetDynamicFromCsvFile(path)) ?? throw new Exception("list was null");
     }
 
     public static async Task<List<dynamic>> GetAdvanced()
     {
         var path =
-            "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/Skills/Advanced.csv_withId";
+            "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/Skills/AdvancedId.csv";
         return await Task.Run(() => CSVHelper.GetDynamicFromCsvFile(path)) ?? throw new Exception("list was null");
     }
 
--- RPGHelper.Functionality/Models/WarhammerFantasy/Talents.cs
+++ RPGHelper.Functionality/Models/WarhammerFantasy/Talents.cs
@@ -8,7 +8,7 @@ public static class Talents
     public static async Task<List<dynamic>> GetTalents()
     {
         var path =
-            "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/Talents.csv_withId";
+            "/home/qnku/RiderProjects/RPGHelper/RPGHelper.Models/Makeshift Excel DB/TalentsId.csv";
         return await Task.Run(()=>CSVHelper.GetDynamicFromCsvFile(path)) ?? throw new Exception("list was null");
     }
     public static async Task<List<dynamic>> GetRandomTalents()
